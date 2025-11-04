7962c6270d3539efca074b139bdf452a852ef1a0
avbrummen <avanbrummen@rhg.com>
2025-11-03 21:35:01 -0800

    add pyproject.toml and source directory

---
+++ src/example_package_avbrummen/vector_mean.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def vector_mean(vector):
+    """arithmetic mean of a series 
+
+    Parameters
+    ----------
+    vector : array-like
+        A vector or series of numbers
+  
+    Returns
+    -------
+    mean
+        Arithmetic mean
+    
+    """
+
+    mean = np.sum(vector) / len(vector)
+
+    return mean
